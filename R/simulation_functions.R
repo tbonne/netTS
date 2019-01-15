b020de2d7e511c197d67c25bf141db5b7e43aadb
tbonne <tyler.bonnell@gmail.com>
2019-01-15 09:41:30 -0700

    added corr.mat options in simulation function

--- R/simulation_functions.R
+++ R/simulation_functions.R
@@ -8,20 +8,47 @@
 #' @importFrom igraph make_full_graph
 #' @importFrom utils setTxtProgressBar txtProgressBar
 #' @importFrom stats runif
+#' @importFrom corpcor make.positive.definite
+#' @importFrom MASS mvrnorm
 #' @export
 #'
-sim.events.data <- function(nodes, sampling.periods, sampling.periods.per.day=1, true.net=NULL){
+sim.events.data <- function(nodes, sampling.periods, sampling.periods.per.day=1, true.net=NULL, ind.probs=NULL, cor.mat=NULL, ind.sd=NULL){
 
+  #setup dataframe to capture simulated data
   day=lubridate::ymd_hms("2002/07/24 00:00:00")
   df.sim <- data.frame(from = 1, to=2, date=day, sampleID=1 )
+
+  #convert correlation matrix to covariance matrix
+  if( !is.null(cor.mat) & !is.null(ind.sd) ){
+    change_vec <- rep(ind.sd,nrow(cor.mat))
+    b <- change_vec %*% t(change_vec)
+    a_covariance <- b * cor.mat
+    a_covariance <- make.positive.definite(a_covariance)
+    print("correlation matrix used for updating node behaivour")
+  }
+
+  #Set probability each individual will show the behaviour
+  if(is.null(ind.probs)){
+    ind.behav <- runif(nodes) #random
+    print("Uniform random draws used for updating node behaivour")
+  } else{
+    ind.behav <- ind.probs #user set
+    if(is.null(cor.mat) | is.null(ind.sd)){
+      print("Fixed node behaivour")
+    }
+  }
 
   #monitor the progress
   pb <- txtProgressBar(min = 1, max = sampling.periods, style = 3)
 
   for(i in 1:sampling.periods){
 
-    #probability each individual will show the behaviour
-    ind.behav <- runif(nodes)
+    #update probability each individual will show the behaviour
+    if(is.null(ind.probs)){
+      ind.behav <- runif(nodes)
+    } else if (!is.null(cor.mat) & !is.null(ind.sd)){
+      ind.behav <- ind.behav +  mvrnorm(1,rep(0,length(ind.behav)),a_covariance)
+    }
 
     #Each individual perform the behaviour
     for(j in 1:length(ind.behav)){
@@ -57,3 +84,4 @@ sim.events.data <- function(nodes, sampling.periods, sampling.periods.per.day=1,
 
   return(df.sim)
 }
+
