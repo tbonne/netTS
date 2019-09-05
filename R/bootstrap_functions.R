d92c40e06e73624322b24c95f3f1cf82fee22087
tbonne <tyler.bonnell@gmail.com>
2019-09-05 12:41:05 -0600

    Updated effort fun and data

--- R/bootstrap_functions.R
+++ R/bootstrap_functions.R
@@ -56,17 +56,19 @@ windowsize.check <- function(data, windowsize=days(30), windowshift=days(1), dir
 #' @param windowshift The amount of time to shift the window when generating networks.
 #' @param directed Whether to consider the network as directed or not (TRUE/FALSE).
 #' @param measureFun The measurment function to perform the bootstap on (should be at the node level).
+#' @param effortFun This is a function that takes as input the data within a window of time and returns the total sampling effort.
+#' @param effortData This is a dataframe containing the data used to calculate sampling effort.
 #' @param corFun The method used to compare observed node/dyad values with bootstrapped values: 1-Cosine similarity, 2-pearsons correlation, 3-Euclidean distance
 #' @param boot.samples The number of bootstrapped samples to run (Default=100)
 #' @param SRI Wether to use the simple ratio index (Default=FALSE)
 #' @param probs The quantiles of the bootrap samples to return (Default=c(0.025,0.975)).
-#' @param fullData If a subset dataset is being used to compare bootstrap samples to the full dataset.
+#' @param fullData This is the full dataset, if a subset dataset is being used to compare bootstrap samples to the full dataset.
 #' @importFrom stats cor.test quantile
 #' @importFrom igraph set_graph_attr degree
 #' @export
 #'
 #'
-convergence.check.boot <- function(data, windowsize=days(30), windowshift=days(1), directed = FALSE, measureFun=degree, corFun = 1,boot.samples=100, SRI=FALSE, probs=c(0.025,0.975), fullData=NULL){
+convergence.check.boot <- function(data, windowsize=days(30), windowshift=days(1), directed = FALSE, measureFun=degree, corFun = 1,boot.samples=100, SRI=FALSE, probs=c(0.025,0.975), effortFun=NULL, effortData=NULL,fullData=NULL){
 
   #intialize times
   windowstart <- min(data[,3])
@@ -84,11 +86,29 @@ convergence.check.boot <- function(data, windowsize=days(30), windowshift=days(1
       df.window<-create.window(data, windowstart, windowend)
       Observation.Events <- nrow(df.window)
 
-      #calculate the desired network measure
-      g <- create.a.network(df.window, directed = directed, SRI=FALSE, effort = 1)
-      g <- set_graph_attr(g, "nEvents", Observation.Events)
-      g <- set_graph_attr(g, "windowstart", windowstart)
-      g <- set_graph_attr(g, "windowend", windowend)
+      #calculate sampling effort
+      if(is.null(effortFun)==FALSE & is.null(effortData)==TRUE  ){ #there is an effort function and it requires no external data
+        g = effortFun(df.window, directed = directed)
+        g <- set_graph_attr(g, "nEvents", Observation.Events)
+        g <- set_graph_attr(g, "windowstart", windowstart )
+        g <- set_graph_attr(g, "windowend", windowend)
+
+      }else if(is.null(effortFun)==FALSE & is.null(effortData)==FALSE ){ #there is an effort function and it requires some external data
+        effortData.sub <- effortData[effortData[,1]>=windowstart & effortData[,1]<windowend,]
+        g = effortFun(df.window, effortData.sub, directed = directed)
+        g <- set_graph_attr(g, "nEvents", Observation.Events)
+        g <- set_graph_attr(g, "windowstart", windowstart )
+        g <- set_graph_attr(g, "windowend", windowend)
+
+      } else { #there is no effort function
+
+        #calculate the desired network measure
+        g <- create.a.network(df.window, directed = directed, SRI=FALSE)
+        g <- set_graph_attr(g, "nEvents", Observation.Events)
+        g <- set_graph_attr(g, "windowstart", windowstart)
+        g <- set_graph_attr(g, "windowend", windowend)
+
+      }
 
       #store measure
       obs.measures <- measureFun(g)
@@ -101,11 +121,29 @@ convergence.check.boot <- function(data, windowsize=days(30), windowshift=days(1
       Observation.Events.full <- nrow(df.window.full)
       Observation.Events <- nrow(df.window)
 
-      #calculate the desired network measure
-      g.full <- create.a.network(df.window.full, directed = directed, SRI=FALSE, effort = 1)
-      g.full <- set_graph_attr(g.full, "nEvents", Observation.Events)
-      g.full <- set_graph_attr(g.full, "windowstart", windowstart)
-      g.full <- set_graph_attr(g.full, "windowend", windowend)
+      #calculate sampling effort
+      if(is.null(effortFun)==FALSE & is.null(effortData)==TRUE  ){ #there is an effort function and it requires no external data
+        g = effortFun(df.window.full, directed = directed)
+        g <- set_graph_attr(g, "nEvents", Observation.Events)
+        g <- set_graph_attr(g, "windowstart", windowstart )
+        g <- set_graph_attr(g, "windowend", windowend)
+
+      }else if(is.null(effortFun)==FALSE & is.null(effortData)==FALSE ){ #there is an effort function and it requires some external data
+        effortData.sub <- effortData[effortData[,1]>=windowstart & effortData[,1]<windowend,]
+        g = effortFun(df.window.full, effortData.sub, directed = directed)
+        g <- set_graph_attr(g, "nEvents", Observation.Events)
+        g <- set_graph_attr(g, "windowstart", windowstart )
+        g <- set_graph_attr(g, "windowend", windowend)
+
+      } else { #there is no effort function
+
+        #calculate the desired network measure
+        g.full <- create.a.network(df.window.full, directed = directed, SRI=FALSE)
+        g.full <- set_graph_attr(g.full, "nEvents", Observation.Events)
+        g.full <- set_graph_attr(g.full, "windowstart", windowstart)
+        g.full <- set_graph_attr(g.full, "windowend", windowend)
+
+      }
 
       #store measure
       obs.measures <- measureFun(g.full)
@@ -116,7 +154,7 @@ convergence.check.boot <- function(data, windowsize=days(30), windowshift=days(1
 
       if(length(obs.measures)<2){
         corFun=3
-       if(corFun!=3)print("Warning: only one measure prduced by the measurement function. corFun set to euclidean distance")
+        if(corFun!=3)print("Warning: only one measure prduced by the measurement function. corFun set to euclidean distance")
       }
 
       #store correlation measures
@@ -128,11 +166,29 @@ convergence.check.boot <- function(data, windowsize=days(30), windowshift=days(1
         #bootstrap sample from the data in this window
         df.sub<-df.window[sample(nrow(df.window),replace = T),]
 
-        #create a network and add it to the list
-        g.boot <- create.a.network(df.sub, directed = directed, SRI=SRI, effort = 1)
-        g.boot <- set_graph_attr(g.boot, "nEvents", Observation.Events)
-        g.boot <- set_graph_attr(g.boot, "windowstart", windowstart)
-        g.boot <- set_graph_attr(g.boot, "windowend", windowend)
+        #calculate sampling effort
+        if(is.null(effortFun)==FALSE & is.null(effortData)==TRUE  ){ #there is an effort function and it requires no external data
+          g = effortFun(df.sub, directed = directed)
+          g <- set_graph_attr(g, "nEvents", Observation.Events)
+          g <- set_graph_attr(g, "windowstart", windowstart )
+          g <- set_graph_attr(g, "windowend", windowend)
+
+        }else if(is.null(effortFun)==FALSE & is.null(effortData)==FALSE ){ #there is an effort function and it requires some external data
+          effortData.sub <- effortData[effortData[,1]>=windowstart & effortData[,1]<windowend,]
+          g = effortFun(df.sub, effortData.sub, directed = directed)
+          g <- set_graph_attr(g, "nEvents", Observation.Events)
+          g <- set_graph_attr(g, "windowstart", windowstart )
+          g <- set_graph_attr(g, "windowend", windowend)
+
+        } else { #there is no effort function
+
+          #create a network and add it to the list
+          g.boot <- create.a.network(df.sub, directed = directed, SRI=SRI)
+          g.boot <- set_graph_attr(g.boot, "nEvents", Observation.Events)
+          g.boot <- set_graph_attr(g.boot, "windowstart", windowstart)
+          g.boot <- set_graph_attr(g.boot, "windowend", windowend)
+
+        }
 
         #take correlation measure
         comb.by.names<-cbind(obs.measures,measureFun(g.boot)[names(obs.measures)])
@@ -153,8 +209,6 @@ convergence.check.boot <- function(data, windowsize=days(30), windowshift=days(1
       #calculate convergence
       conv.values <- rbind(conv.values,data.frame(mean=NA,CI.low=NA,CI.high=NA,windowstart=windowstart, windowend=windowend))
     }
-
-
 
     #move the window
     windowend = windowend + windowshift
